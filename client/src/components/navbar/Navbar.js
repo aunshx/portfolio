dd897fa81a8b89986461d973700259728120db8f
aunshx <aunsh.sb@gmail.com>
2022-02-06 12:08:38 +0530

    navbar animate

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Drawer, Tooltip } from "@mui/material";
+
+import SidebarMini from './SidebarMini'
+
+import windowSize from '../../utils/windowSize'
 
 const Navbar = (props) => {
-  return <div className='navbar flex_between'>
-    <div className="left">
-      aunsh.
+  const [menu, setMenu] = useState(false)
+  const [drawer, setDrawer] = useState(false);
+
+  const { width, height }  = windowSize()
+
+  const verticalMenu = () => {
+    setMenu(!menu)
+    setDrawer(!drawer);
+  };
+  return <>
+  <div className='navbar flex_between'>
+    <div className="cursor_pointer">
+      <NavLink to='/'>
+        <div className="left">
+          aunsh.
+        </div>
+      </NavLink>
     </div>
     <div className="right flex_between">
       <NavLink to='/'>
@@ -28,7 +49,57 @@ const Navbar = (props) => {
         Resume
       </div>
     </div>
-  </div>;
+      {width < 787 && (
+        <div>
+        {menu ? (
+            <div className='flex_middle'>
+              <Tooltip title='Menu' placement='left'>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faBars}
+                    className={"mobile_logo--tilted"}
+                    onClick={verticalMenu}
+                    style={{
+                      fontSize: 17,
+                    }}
+                  />
+                </div>
+              </Tooltip>
+            </div>
+          ) : (
+            <div className='flex_middle'>
+              <Tooltip title='Menu' placement='left'>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faBars}
+                    className={"mobile_logo"}
+                    onClick={verticalMenu}
+                    style={{
+                      fontSize: 17,
+                    }}
+                  />
+                </div>
+              </Tooltip>
+            </div>
+          )}
+          <div>
+      </div>
+      </div>
+      )}
+    </div>
+  <div>
+    {width < 787 && (
+    <Drawer
+          anchor={"right"}
+          open={drawer}
+          onClose={verticalMenu}
+          className='sidebar_nav-right'
+        >
+          <SidebarMini open={verticalMenu} />
+        </Drawer>
+  )}
+  </div>
+  </>
 };
 
 Navbar.propTypes = {};
