791b7487ddd5c0b8f35f5c28156dd8e78aa517f0
aunshx <aunsh.sb@gmail.com>
2022-02-06 11:18:12 +0530

    navlink added

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -9,11 +9,21 @@ const Navbar = (props) => {
       aunsh.
     </div>
     <div className="right flex_between">
-       <div className='element cursor_pointer'>Home</div>
+      <NavLink to='/'>
+         <div className='element cursor_pointer'>Home</div>
+      </NavLink>
+      <NavLink to='/projects'>
       <div className='element cursor_pointer'>Projects</div>
-      <div className='element cursor_pointer'>Articles</div>
-      <div className='element cursor_pointer'>Skills</div>
-      <div className='element cursor_pointer'>Contact</div>
+      </NavLink>
+      <NavLink to='/articles'>
+         <div className='element cursor_pointer'>Articles</div>
+      </NavLink>
+      <NavLink to='/skills'>
+         <div className='element cursor_pointer'>Skills</div>
+      </NavLink>
+      <NavLink to='/contacts'>
+         <div className='element cursor_pointer'>Contact</div>
+      </NavLink>
       <div className="resume">
         Resume
       </div>
