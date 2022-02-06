f80d938ad1a27fb533d5c54e9187584da66287be
aunshx <aunsh.sb@gmail.com>
2022-02-06 12:19:55 +0530

    sidebar nsv

--- client/src/components/navbar/SidebarMini.js
+++ client/src/components/navbar/SidebarMini.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import CloseIcon from '@mui/icons-material/Close';
+
 const SidebarMini = (props) => {
-  return <div>
-      vkvkvkvkvkvkvkvk
+  return <div className='sidebar'>
+      <div className="title triple_grid">
+        <div></div>
+        <div className="text">
+          Menu
+        </div>
+        <div className="icon cursor_pointer flex_middle">
+          <CloseIcon style={{ fontSize: 16, marginTop: '0.1em' }} className='cancel' />
+        </div>
+      </div>
   </div>;
 };
 
