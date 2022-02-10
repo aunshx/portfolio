f0463b00c4b91de8fbfe5ca3a0667bc32305bc20
aunshx <aunsh.sb@gmail.com>
2022-02-10 19:06:16 +0530

    disappear dialog

--- client/src/components/contact/Me.js
+++ client/src/components/contact/Me.js
@@ -11,14 +11,24 @@ import Background from '../main/Background';
 import body from '../../resources/images/onlyBodyedit.png'
 import body1 from '../../resources/images/onlyBody2.png'
 import body2 from '../../resources/images/onlyBody3.png'
+import { useState } from 'react';
 
 const Me = (props) => {
+    const [showDialog, setShowDialog] = useState(true)
+
+    const changeDialog = () => {
+        setShowDialog(!showDialog)
+    }
   return<>
-      <div className="all-about">
-      <div class="bubble bubble-bottom-left">Send <span style={{ display: 'inline-block', color: 'rgb(0, 145, 255)' }}>me</span> an email and I'll <span style={{ display: 'inline-block', color: 'rgb(0, 145, 255)' }}>definitely</span> get <span style={{ display: 'inline-block', color: 'rgb(0, 145, 255)' }}>back</span> to you.</div>
+      <div className="all-about" onClick={changeDialog}>
+      {showDialog ? (
+          <>
+              <div class="bubble bubble-bottom-left">Send <span style={{ display: 'inline-block', color: 'rgb(0, 145, 255)' }}>me</span> an email and I'll <span style={{ display: 'inline-block', color: 'rgb(0, 145, 255)' }}>definitely</span> get <span style={{ display: 'inline-block', color: 'rgb(0, 145, 255)' }}>back</span> to you.</div>
            <div className='image'>
          <img src={body} alt="Face of Me" />
      </div>
+          </>
+      ) : ('')}
       </div>
   </>
 };
