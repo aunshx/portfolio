e0552c31989d9987eaddac36bce9d5f89da2f6f2
aunshx <aunsh.sb@gmail.com>
2022-02-18 20:07:10 +0530

    retrieve oldest messages

--- routes/api/contact.js
+++ routes/api/contact.js
@@ -5,8 +5,37 @@ const { check, validationResult } = require("express-validator")
 const Message = require('../../models/Message')
 const auth = require('../../middleware/auth')
 
-// @route    GET api/contact
-// @desc     Get contacts
+// @route    POST api/contact
+// @desc     Get Messages Oldest
+// @access   Private
+router.post(
+  "/retrieve-messages-oldest",
+  auth,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const { skipNow } = req.body
+
+    try {
+      let ans = await Message.find({ userId: req.user.id })
+        .sort({
+          createdAt: 1,
+        })
+        .skip(skipNow)
+        .limit(30);
+
+      return res.status(200).send(ans);
+    } catch (err) {
+      res.status(400).send({ errors: [{ msg: "Cannot retrieve messages" }] });
+    }
+  }
+);
+
+// @route    POST api/contact
+// @desc     Get Messages
 // @access   Private
 router.post(
   "/retrieve-messages-latest",
