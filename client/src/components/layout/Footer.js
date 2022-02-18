ce41d7d2aa9cb850a5066a6884b4c170b7d9859b
aunshx <aunsh.sb@gmail.com>
2022-02-18 11:10:04 +0530

    footer linking

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Footer = ({  }) => {
   return (
     <div className='footer flex_middle'>
-        <div className="text">
-           <span style={{ marginBottom: '0.5em', fontSize: '0.8em' }}>{String.fromCodePoint('0X00A9')}</span> aunsh.
-         <span style={{ fontSize: '0.8em', fontWeight: '400' }}> 2022</span>
+        <div className='text cursor_pointer'>
+          <span style={{ marginBottom: "0.5em", fontSize: "0.8em" }}>
+            {String.fromCodePoint("0X00A9")}
+          </span>{" "}
+          <a href='https://admin.aunsh.com' target={'_blank'} rel='noreferrer nofollow'>aunsh.</a>
+          <span style={{ fontSize: "0.8em", fontWeight: "400" }}> 2022</span>
         </div>
     </div>
   );
