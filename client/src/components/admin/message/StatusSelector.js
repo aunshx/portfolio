3b887641e244780f2dd069755b5bd19f0cdc53ee
aunshx <aunsh.sb@gmail.com>
2022-02-19 15:15:34 +0530

    dropdown edit

--- client/src/components/admin/message/StatusSelector.js
+++ client/src/components/admin/message/StatusSelector.js
@@ -4,6 +4,24 @@ import { Menu, MenuItem, Tooltip } from '@mui/material';
 import { connect } from 'react-redux';
 
 const StatusSelector = ({ anchorEl, open, handleClose, settings: { displayMode } }) => {
+
+    const changeStatus  = (stat) => {
+        if(stat === 'success') {
+            
+        }
+        if(stat === 'ongoing') {
+            
+        }
+        if(stat === 'cold') {
+            
+        }
+        if(stat === 'unseen') {
+            
+        }
+        if(stat === 'not-replied') {
+            
+        }
+    }
   return (
     <>
       {displayMode ? (
@@ -18,6 +36,87 @@ const StatusSelector = ({ anchorEl, open, handleClose, settings: { displayMode }
               overflow: "visible",
               filter: "drop-shadow(0px 2px 4px rgba(0,0,0,0.32))",
               backgroundColor: "rgb(50,50,50)",
+              width: "50px",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+            },
+          }}
+          transformOrigin={{
+            horizontal: "center",
+            vertical: "top",
+          }}
+          anchorOrigin={{
+            horizontal: "center",
+            vertical: "bottom",
+          }}
+        >
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("not-replied")}
+          >
+            <Tooltip title='No Reply' placement='left'>
+              <div className='tag-small flex_middle'>
+                <div className='not-replied'>NR</div>
+              </div>
+            </Tooltip>
+          </div>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("unseen")}
+          >
+            <Tooltip title='Unseen' placement='left'>
+              <div className='tag-small flex_middle'>
+                <div className='unseen'>UN</div>
+              </div>
+            </Tooltip>
+          </div>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("ongoing")}
+          >
+            <Tooltip title='Ongoing' placement='left'>
+              <div className='tag-small flex_middle'>
+                <div className='ongoing'>ON</div>
+              </div>
+            </Tooltip>
+          </div>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("cold")}
+          >
+            <Tooltip title='Cold' placement='left'>
+              <div className='tag-small flex_middle'>
+                <div className='cold'>CD</div>
+              </div>
+            </Tooltip>
+          </div>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("success")}
+          >
+            <Tooltip title='Success' placement='left'>
+              <div className='tag-small flex_middle'>
+                <div className='success'>SC</div>
+              </div>
+            </Tooltip>
+          </div>
+        </Menu>
+      ) : (
+        <Menu
+          id='fade-menu'
+          anchorEl={anchorEl}
+          open={open}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 4px rgba(0,0,0,0.32))",
               width: '50px',
               mt: 1.5,
               "& .MuiAvatar-root": {
@@ -37,106 +136,56 @@ const StatusSelector = ({ anchorEl, open, handleClose, settings: { displayMode }
             vertical: "bottom",
           }}
         >
-          <div className='tag-small-menu flex_middle'>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("not-replied")}
+          >
             <Tooltip title='No Reply' placement='left'>
               <div className='tag-small flex_middle'>
                 <div className='not-replied'>NR</div>
               </div>
             </Tooltip>
           </div>
-          <div className='tag-small-menu flex_middle'>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("unseen")}
+          >
             <Tooltip title='Unseen' placement='left'>
               <div className='tag-small flex_middle'>
                 <div className='unseen'>UN</div>
               </div>
             </Tooltip>
           </div>
-          <div className='tag-small-menu flex_middle'>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("ongoing")}
+          >
             <Tooltip title='Ongoing' placement='left'>
               <div className='tag-small flex_middle'>
                 <div className='ongoing'>ON</div>
               </div>
             </Tooltip>
           </div>
-          <div className='tag-small-menu flex_middle'>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("cold")}
+          >
             <Tooltip title='Cold' placement='left'>
               <div className='tag-small flex_middle'>
                 <div className='cold'>CD</div>
               </div>
             </Tooltip>
           </div>
-          <div className='tag-small-menu flex_middle'>
+          <div
+            className='tag-small-menu flex_middle'
+            onClick={() => changeStatus("success")}
+          >
             <Tooltip title='Success' placement='left'>
               <div className='tag-small flex_middle'>
                 <div className='success'>SC</div>
               </div>
             </Tooltip>
           </div>
-        </Menu>
-      ) : (
-        <Menu
-          id='fade-menu'
-          anchorEl={anchorEl}
-          open={open}
-          onClick={handleClose}
-          PaperProps={{
-            elevation: 0,
-            sx: {
-              overflow: "visible",
-              filter: "drop-shadow(0px 2px 4px rgba(0,0,0,0.32))",
-              mt: 1.5,
-              "& .MuiAvatar-root": {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-              },
-            },
-          }}
-          transformOrigin={{
-            horizontal: "center",
-            vertical: "top",
-          }}
-          anchorOrigin={{
-            horizontal: "center",
-            vertical: "bottom",
-          }}
-        >
-          <MenuItem>
-            <Tooltip title='No Reply' placement='left'>
-              <div className='tag-small'>
-                <div className='not-replied'>NR</div>
-              </div>
-            </Tooltip>
-          </MenuItem>
-          <MenuItem>
-            <Tooltip title='Unseen' placement='left'>
-              <div className='tag-small'>
-                <div className='unseen'>UN</div>
-              </div>
-            </Tooltip>
-          </MenuItem>
-          <MenuItem>
-            <Tooltip title='Ongoing' placement='left'>
-              <div className='tag-small'>
-                <div className='ongoing'>ON</div>
-              </div>
-            </Tooltip>
-          </MenuItem>
-          <MenuItem>
-            <Tooltip title='Cold' placement='left'>
-              <div className='tag-small'>
-                <div className='cold'>CD</div>
-              </div>
-            </Tooltip>
-          </MenuItem>
-          <MenuItem>
-            <Tooltip title='Success' placement='left'>
-              <div className='tag-small'>
-                <div className='success'>SC</div>
-              </div>
-            </Tooltip>
-          </MenuItem>
         </Menu>
       )}
     </>
