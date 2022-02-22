a84f058f2fba66a0201ae0795a89ee9a88a05d30
aunshx <aunsh.sb@gmail.com>
2022-02-22 15:42:12 +0530

    pul down to refresh

--- client/src/components/admin/stats/Stats.js
+++ client/src/components/admin/stats/Stats.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import PullToRefresh from "react-simple-pull-to-refresh";
 
 import Navbar from '../navbar/Navbar'
 import { connect } from 'react-redux';
@@ -16,6 +17,9 @@ import TotalNotReplied from './blocks/lower/TotalNotReplied';
 import SynopsisHits from './blocks/upper/SynopsisHits';
 import RecentEmails from './blocks/mid/RecentEmails';
 import { Button } from '@mui/material';
+import Spinner from "../../layout/Spinner";
+
+import RefreshIcon from "@mui/icons-material/Refresh"
 
 import {getTotalHitsToday} from '../../../redux/actions/metrics'
 
@@ -28,102 +32,116 @@ const Stats = ({}) => {
     setChange(true)
   }, [change])
 
-  const letsReload = () => {
-    setChange(false)
+  const refreshingFeed = (offset) => {
+    setChange(false);
+    return new Promise((resolve) => {
+      resolve();
+    });
   };
 
   return (
     <>
       <Navbar />
-      {change && (
-        <div className='app'>
-          <div className={width < 787 ? "stats-main-mobile" : "stats-main"}>
-            <div
-              className='one flex_middle'
-              style={{ border: "1px solid red" }}
-              data-aos='fade-up'
-              data-aos-delay={100}
-            >
-              <VisitorGraph />
-            </div>
-            <div className='two' style={{ border: "1px solid red" }}>
+      {change ? (
+        <PullToRefresh
+          onRefresh={refreshingFeed}
+          refreshingContent={false}
+          pullingContent={'Refreshing...'}
+        >
+          <div className='app'>
+            <div className={'grabbing'} style={{ marginTop: '45px' , width: '100%', height: '20px'}}></div>
+            <div className={width < 787 ? "stats-main-mobile" : "stats-main"} style={{ marginTop: '0' }}>
               <div
-                className='two-one'
+                className='one flex_middle'
                 style={{ border: "1px solid red" }}
                 data-aos='fade-up'
                 data-aos-delay={100}
               >
-                <TotalHits />
+                <VisitorGraph />
+              </div>
+              <div className='two' style={{ border: "1px solid red" }}>
+                <div
+                  className='two-one'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={100}
+                >
+                  <TotalHits />
+                </div>
+                <div
+                  className='two-two'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={200}
+                >
+                  <SynopsisHits />
+                </div>
+                <div
+                  className='two-three'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={300}
+                >
+                  <RecentEmails />
+                </div>
+                <div
+                  className='two-four'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={400}
+                >
+
+                </div>
               </div>
               <div
-                className='two-two'
+                className='three'
                 style={{ border: "1px solid red" }}
                 data-aos='fade-up'
                 data-aos-delay={200}
               >
-                <SynopsisHits />
+                <HitsChart />
               </div>
-              <div
-                className='two-three'
-                style={{ border: "1px solid red" }}
-                data-aos='fade-up'
-                data-aos-delay={300}
-              >
-                <RecentEmails />
-              </div>
-              <div
-                className='two-four'
-                style={{ border: "1px solid red" }}
-                data-aos='fade-up'
-                data-aos-delay={400}
-              >
-                <Button style={{ color: "red" }} onClick={() => letsReload()}>
-                  Click to reload!
-                </Button>
-              </div>
-            </div>
-            <div
-              className='three'
-              style={{ border: "1px solid red" }}
-              data-aos='fade-up'
-              data-aos-delay={200}
-            >
-              <HitsChart />
-            </div>
-            <div className='four' style={{ border: "1px solid red" }}>
-              <div
-                className='four-one'
-                style={{ border: "1px solid red" }}
-                data-aos='fade-up'
-                data-aos-delay={500}
-              >
-                <TotalSuccess />
-              </div>
-              <div
-                className='four-two'
-                style={{ border: "1px solid red" }}
-                data-aos='fade-up'
-                data-aos-delay={600}
-              >
-                <TotalOngoings />
-              </div>
-              <div
-                className='four-three'
-                style={{ border: "1px solid red" }}
-                data-aos='fade-up'
-                data-aos-delay={700}
-              >
-                <TotalNotReplied />
-              </div>
-              <div
-                className='four-four'
-                style={{ border: "1px solid red" }}
-                data-aos='fade-up'
-                data-aos-delay={800}
-              >
-                <TotalColds />
+              <div className='four' style={{ border: "1px solid red" }}>
+                <div
+                  className='four-one'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={500}
+                >
+                  <TotalSuccess />
+                </div>
+                <div
+                  className='four-two'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={600}
+                >
+                  <TotalOngoings />
+                </div>
+                <div
+                  className='four-three'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={700}
+                >
+                  <TotalNotReplied />
+                </div>
+                <div
+                  className='four-four'
+                  style={{ border: "1px solid red" }}
+                  data-aos='fade-up'
+                  data-aos-delay={800}
+                >
+                  <TotalColds />
+                </div>
               </div>
             </div>
+          </div>
+        </PullToRefresh>
+      ) : (
+        <div className='flex_middle'>
+          <div className='not-found flex_middle'>
+            <Spinner />
           </div>
         </div>
       )}
