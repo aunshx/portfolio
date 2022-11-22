446967ac098528e44ba289409aa2c1df8f69de96
aunshx <aunsh.sb@gmail.com>
2022-11-22 12:20:24 +0530

    Footer changes

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -7,20 +7,21 @@ const Footer = ({  }) => {
     <div className='footer flex_middle'>
       <div className='text cursor_pointer'>
         <span style={{ marginBottom: "0.5em", fontSize: "0.8em" }}>
-          Handcrafted with &#10084; |
+          Handcrafted with &#10084; by{" "}
+          <a
+            href='https://admin.aunsh.com/login'
+            target={"_blank"}
+            rel='noreferrer nofollow'
+            style={{ fontSize: "0.95em", marginRight: "0.3em" }}
+          >
+            Aunsh B
+          </a>{" "}
+          |
         </span>{" "}
         <span style={{ margin: "0.3em 0.3em 0 0", fontSize: "0.8em" }}>
           {" "}
           {String.fromCodePoint("0X00A9")}
         </span>
-        <a
-          href='https://admin.aunsh.com/login'
-          target={"_blank"}
-          rel='noreferrer nofollow'
-          style={{ fontSize: "0.85em", marginRight: "0.3em" }}
-        >
-          aunsh.
-        </a>
         <span style={{ fontSize: "0.8em", fontWeight: "400" }}>2022</span>
       </div>
     </div>
