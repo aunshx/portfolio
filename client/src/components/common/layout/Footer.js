7138e63fbfc8df3f599a0504210a25e354a4cdc9
aunshx <aunsh.sb@gmail.com>
2022-11-26 11:55:36 +0530

    Build v2.0.1

--- client/src/components/common/layout/Footer.js
+++ client/src/components/common/layout/Footer.js
@@ -16,11 +16,10 @@ const Footer = ({  }) => {
           >
             Aunsh B
           </a>{" "}
-          &middot;
         </span>
       </div>
       <div className='text' style={{ fontSize: "0.55em", fontWeight: "200", marginTop: '0.5em' }}>
-        v2.0.1 &middot; Update 26 Nv 22 &middot;
+        v2.0.1 &middot; Update 26 Nov 22 &middot;
         All rights reserved
       </div>
     </div>
